6d321cdf78c322b2f1bbd5005e85139631656f27
IDM014 <IDM014@IDM012s-Mac-mini.local>
2018-12-12 16:43:42 +0530

    changes frmaework

---
+++ SDK/SelfieCocoapod.framework/Headers/SelfieCocoapod.h
@@ -0,0 +1,19 @@
+//
+//  SelfieCocoapod.h
+//  SelfieCocoapod
+//
+//  Created by IDM014 on 12/12/18.
+//  Copyright © 2018 e. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SelfieCocoapod/DataViewController.h>
+//! Project version number for SelfieCocoapod.
+FOUNDATION_EXPORT double SelfieCocoapodVersionNumber;
+
+//! Project version string for SelfieCocoapod.
+FOUNDATION_EXPORT const unsigned char SelfieCocoapodVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SelfieCocoapod/PublicHeader.h>
+
+
---
+++ SelfieCocoapod.podspec
@@ -0,0 +1,18 @@
+Pod::Spec.new do |s|
+
+s.name         = "SelfieCocoapod"
+s.version      = "1.0.0"
+s.summary      = "Testing sdk with the cocoapods abcdefghijklmnop"
+s.description  = "validation of the cards for the given image abc"
+s.homepage     = "https://github.com/KrishnaMohan454/SelfieCapture.git"
+s.license      = "MIT"
+s.author       = "Krishna"
+s.platform     = :ios
+s.source       = { :git => "https://github.com/KrishnaMohan454/SelfieCapture.git", :tag => "1.0.0" }
+
+s.ios.deployment_target  = '10.0'
+s.ios.vendored_framework = 'SDK/SelfieCocoapod.framework'
+
+
+end
+
